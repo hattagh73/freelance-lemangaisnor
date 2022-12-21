c657ea93f29f4a2b6a8a56ce0001bec2b602dce6
hattagh73 <>
2022-12-22 00:17:21 +0800

    add: tailwind-config

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type { import("tailwindcss").Config } */
+
+tailwindConfig = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./styles/**/*.css"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "clr-light-primary": "#FAFAFA",
+                "clr-light-secondary": "#F5F5F5",
+                "clr-light-tertiary": "#28553B",
+                "clr-green-primary": "#24C166",
+                "clr-green-seondary": "#21B45E",
+                "clr-green-tertiary": "#1EA657",
+                "clr-dark-primary": "#0A371D",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            fontSize: {
+                "10xl": ["10rem", { lineHeight: "1" }],
+            },
+            screens: {
+                "2xl": "1537px",
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
+
+module.exports = tailwindConfig;
