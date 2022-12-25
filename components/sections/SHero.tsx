54ac785dff2dfd4a535816768f8384befc276327
hattagh73 <>
2022-12-25 13:42:03 +0800

    update: component-hero

--- components/sections/SHero.tsx
+++ components/sections/SHero.tsx
@@ -8,6 +8,7 @@ import { iSHero } from '../../config/data-type';
 import { motion } from 'framer-motion';
 import { staggerContainer, fadeInDown, ketupat, light } from '../../config/framer-variants';
 
+// IMPORT: classnames-package
 import classNames from 'classnames';
 
 //* START: section-hero
@@ -18,25 +19,36 @@ const SHero = ({hero}: iSHero) => {
             initial="initial"
             animate="animate"
             className={classNames(
-                // "bg-clr-light-secondary",
+                "bg-clr-light-secondary relative",
                 "text-clr-light-primary",
                 "pt-6",
-                "lg:pt-36 lg:pb-28",
+                "lg:pt-0 lg:pb-0",
             )}
         >
+            {/* layer-img-lemang */}
+            <Image 
+                alt="Hero Image"
+                src="https://res.cloudinary.com/dltslq5fh/image/upload/v1671904279/aisnor/dnlb43kumipkhdz4bbb5.jpg"
+                fill
+                className="absolute -z-0"
+            />
+
+            {/* layer-overlay */}
+            <div className="absolute inset-0 -z-0 bg-black/70"/>
 
             {/* START: hero-wrap-grid */}
             <div
-                
-                
                 className={classNames(
-                    "c-container bg-clr-green-primary rounded-3xl",
-                    "py-6 px-6", "lg:py-24 lg:px-10",
+                    "c-container",
+                    // "bg-clr-green-primary",
+                    "rounded-lg",
+                    "py-6 px-6", "lg:py-44 lg:px-10",
                     // "grid grid-cols-1", "lg:place-items-center",
-                    "relative",
+                    "relative z-0",
                     // "shadow-lg shadow-clr-dark-primary/10"
                 )}
             >
+
                 <motion.div variants={fadeInDown} className="grid grid-cols-1 lg:place-items-center">
                     <h1
                         
@@ -52,6 +64,7 @@ const SHero = ({hero}: iSHero) => {
                         
                         className={classNames(
                             "my-12", "lg:text-center lg:my-16",
+                            "lg:text-lg",
                             "lg:max-w-[40%]"
                         )}  
                     >{hero.desc}
@@ -60,16 +73,17 @@ const SHero = ({hero}: iSHero) => {
                     <button
                         
                         className={classNames(
-                            "rounded-lg py-3 px-16 font-medium",
-                            // "bg-clr-light-secondary text-clr-green-primary"
+                            "rounded-md py-3 px-16 font-medium",
+                            // "bg-clr-green-primary text-clr-light-primary",
                             "border-2 border-clr-light-primary"
                             
                         )}  
                     >{hero.info}
                     </button>
                 </motion.div>
+
                 {/* img-light */}
-                <motion.span
+                {/* <motion.span
                     variants={light}
                     className="absolute bottom-0 left-[2%] hidden lg:inline w-48 h-48"
                 >
@@ -79,10 +93,10 @@ const SHero = ({hero}: iSHero) => {
                         fill
                         sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw"
                     />
-                </motion.span>
+                </motion.span> */}
                 
                 {/* img-ketupat */}
-                <motion.span
+                {/* <motion.span
                     variants={ketupat}
                     className="absolute top-0 right-[2%] w-14 h-14 lg:w-48 lg:h-48"
                 >
@@ -92,113 +106,35 @@ const SHero = ({hero}: iSHero) => {
                         fill
                         sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw"
                     />
-                </motion.span>
+                </motion.span> */}
 
             </div>{/* END: hero-wrap-grid */}
+
+            <div className={classNames(
+                // "border border-clr-green-primary",
+                "py-4 px-7",
+                "relative z-0 flex",
+                "flex-row justify-between",
+                "text-clr-light-primary text-xs font-light",
+                "bg-black/10 backdrop-blur-xl"
+            )}>
+
+                <p className="">Lemang Aisnor</p>
+
+                <ul className=" flex gap-x-7">
+                    {["Facebook", "Instragram", "WhatsApp"].map((i,index) =>
+                        <li
+                            key={index}
+                        >{i}</li>
+                    )}
+                </ul>
+
+                <p className="">Sejak 2020</p>
+
+            </div>
 
         </motion.section>
     )
 }
 export default SHero;
-//* END: section-hero
-
-// //* IMPORT: nextjs-features-types
-// import Image from 'next/image';
-
-// //* IMPORT: types
-// import { iSHero } from '../../config/data-type';
-
-// //* IMPORT: framer-motion
-// import { motion } from 'framer-motion';
-// import { staggerContainer, fadeInDown, ketupat, light } from '../../config/framer-variants';
-
-// import classNames from 'classnames';
-
-//* START: section-hero
-// const SHero = ({hero}: iSHero) => {
-//     return (
-//         <section
-//             className={classNames(
-//                 "text-clr-light-primary bg-clr-light-secondary",
-//                 "pt-6",
-//                 "lg:pt-36 lg:pb-28",
-//             )}
-//         >
-
-//             {/* START: hero-wrap-grid */}
-//             <motion.div
-//                 variants={staggerContainer}
-//                 initial="initial"
-//                 animate="animate"
-//                 className={classNames(
-//                     "c-container bg-clr-green-primary rounded-3xl",
-//                     "py-6 px-6", "lg:py-24 lg:px-10",
-//                     "grid grid-cols-1", "lg:place-items-center",
-//                     "relative",
-//                     // "shadow-lg shadow-clr-dark-primary/10"
-//                 )}
-//             >
-
-//                 <motion.h1
-//                     variants={fadeInDown}
-//                     className={classNames(
-//                         "font-semibold",
-//                         "text-3xl",
-//                         "lg:text-5xl"
-//                     )}  
-//                 >{hero.title}
-//                 </motion.h1>
-                
-//                 <motion.p
-//                     variants={fadeInDown}
-//                     className={classNames(
-//                         "my-12", "lg:text-center lg:my-16",
-//                         "lg:max-w-[40%]"
-//                     )}  
-//                 >{hero.desc}
-//                 </motion.p>
-
-//                 <motion.button
-//                     variants={fadeInDown}
-//                     className={classNames(
-//                         "rounded-lg py-3 px-16 font-medium",
-//                         // "bg-clr-light-secondary text-clr-green-primary"
-//                         "border-2 border-clr-light-primary"
-                        
-//                     )}  
-//                 >{hero.info}
-//                 </motion.button>
-
-//                 {/* img-light */}
-//                 <motion.span
-//                     variants={light}
-//                     className="absolute bottom-0 left-[2%] drop-shadow-md hidden lg:inline w-48 h-48"
-//                 >
-//                     <Image 
-//                         alt="Illustration Light SVG"
-//                         src={'/images/light.svg'}
-//                         fill
-//                         sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw"
-//                     />
-//                 </motion.span>
-                
-//                 {/* img-ketupat */}
-//                 <motion.span
-//                     variants={ketupat}
-//                     className="absolute top-0 right-[2%] drop-shadow-md w-14 h-14 lg:w-48 lg:h-48"
-//                 >
-//                     <Image 
-//                         alt="Illustration Ketupat SVG"
-//                         src={'/images/ketupat.svg'}
-//                         fill
-//                         sizes="(max-width: 768px) 100vw,(max-width: 1200px) 50vw,33vw"
-//                     />
-//                 </motion.span>
-
-//             </motion.div>{/* END: hero-wrap-grid */}
-
-//         </section>
-//     )
-// }
-// export default SHero;
 //* END: section-hero
