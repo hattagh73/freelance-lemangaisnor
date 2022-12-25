7910769e8ff55535d0c5522aeb7b99dba882c287
hattagh73 <>
2022-12-25 13:42:11 +0800

    update: component-product

--- components/sections/SProduct.tsx
+++ components/sections/SProduct.tsx
@@ -1,13 +1,14 @@
 //* IMPORT: nextjs-features-types
 import Image from 'next/image';
 
-//* IMPORT: types
+//* IMPORT: data-types
 import { iSProduct } from '../../config/data-type';
 
 //* IMPORT: framer-motion
 import { motion } from 'framer-motion';
 import { staggerContainer, fadeInDown, ketupat, light } from '../../config/framer-variants';
 
+//* IMPORT: classnames-package
 import classNames from 'classnames';
 
 //* START: section-product
@@ -16,41 +17,77 @@ const SProduct = ({product}: iSProduct) => {
         <section
             id="s_contact"
             className={classNames(
-                "c-container",
-                "pt-32 pb-32",
-                "grid grid-cols-2",
+                "pt-32 pb-72",
+                "bg-clr-light-secondary"
             )}
         >
-
+            
+            {/* START: product-container */}
             <div
                 className={classNames(
-                    "order-2 text-end"
+                    "c-container",
+                    "grid grid-cols-2",
+                    "gap-x-24"
                 )}
             >
 
-                <h1
+                {/* START: product-col-1 */}
+                <div
                     className={classNames(
-                        "text-2xl font-semibold"
+                        "order-2 flex flex-col items-end text-end"
                     )}
-                >{product.title}
-                </h1>
+                >
+                    <h4 className="mini-title">
+                        {product.mini_title}
+                    </h4>
 
-                <p
+                    <h1
+                        className={classNames(
+                            "main-title"
+                        )}
+                    >{product.title}
+                    </h1>
+
+                    <p
+                        className={classNames(
+                            "mt-10 text-clr-light-tertiary max-w-[80%] "
+                        )}
+                    >{product.desc}
+                    </p>
+
+                </div>{/* END: product-col-1 */}
+                
+                {/* START: product-col-2 */}
+                <div
                     className={classNames(
-                        "mt-10 text-clr-light-tertiary"
+                        "order-1",
+                        "grid grid-cols-2 grid-rows-[200px_200px]",
+                        "gap-1",
                     )}
-                >{product.desc}
-                </p>
+                >
+                    {   product.img && product.img.map(i =>
+                        <div
+                            key={i.id}
+                            className={classNames(
+                                "border rounded-lg relative overflow-hidden",
+                                "bg-clr-light-secondary",
+                                {"col-span-2":i.id === 3},
+                            )}
+                        >
+                            <div
+                                className="absolute z-[2] inset-0 hover:bg-black/25"
+                            ></div>
+                            <Image 
+                                alt="Gambar Produk"
+                                src={i.src}
+                                fill
+                                className="object-cover z-[1]"
+                            />
+                        </div>
+                    )}
+                </div>{/* END: product-col-2 */}
 
-            </div>
-
-            <div
-                className={classNames(
-                    "order-1"
-                )}
-            >
-                about-col-2
-            </div>
+            </div>{/* END: product-container */}
 
         </section>
     )
