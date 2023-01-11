8691a4bc6cca4202b4795a0d5690bd68c3b3bdf7
hattagh73 <>
2023-01-11 22:32:17 +0800

    UPDATE: design-testimony

--- components/sections/STestimony.tsx
+++ components/sections/STestimony.tsx
@@ -11,6 +11,10 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
+//* IMPORT: framer-motion
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeInDown, fadeInUpwards } from '../../config/framer-variants';
+
 //* IMPORT: classnames
 import classNames from 'classnames';
 
@@ -21,14 +25,19 @@ const STestimony = ({testimony}: iSTestimony ) => {
 
     return (
 
-        <section
+        <motion.section
+            variants={staggerContainer}
+            initial="initial"
+            whileInView={"animate"}
+            viewport={{once:true, amount: 0.6}}
             id="s_testimony"
             className={classNames(
-                "pt-32 pb-72",
+                "py-20",
             )}
         >
             {/* START: container */}
-            <div
+            <motion.div
+                variants={fadeInDown}
                 className={classNames(
                     "c-container grid",
                     "grid-cols-12 gap-x-5",
@@ -60,51 +69,53 @@ const STestimony = ({testimony}: iSTestimony ) => {
                         "mt-28 col-span-12 flex flex-col items-center text-center"
                     )}
                 > */}
-                    <Swiper
-                        pagination={{
-                            enabled: true,
-                            clickable: true,
-                            bulletActiveClass: `${bulletActiveClass}`,
-                        }}
-                        modules={[Pagination, Navigation]}
-                        className="mt-28 col-span-12 max-w-full pb-10 px-3"
-                    >
-                        {testimony.testimonials.map(i =>
-                            <SwiperSlide 
-                                key={i.id}
-                                className="flex flex-col items-center text-center"
+
+                <Swiper
+                    pagination={{
+                        enabled: true,
+                        clickable: true,
+                        bulletActiveClass: `${bulletActiveClass}`,
+                    }}
+                    modules={[Pagination, Navigation]}
+                    className="mt-28 col-span-12 max-w-full pb-6 px-3"
+                >
+                    {testimony.testimonials.map(i =>
+                        <SwiperSlide 
+                            key={i.id}
+                            className="flex flex-col items-center text-center"
+                        >
+                            {/* user-testimony */}
+                            <p
+                                className={classNames(
+                                    "my-10 text-clr-light-tertiary order-3",
+                                    "lg:max-w-[55%]"
+                                )}
                             >
-                                {/* user-testimony */}
-                                <p
-                                    className={classNames(
-                                        "my-10 text-clr-light-tertiary order-3",
-                                        "lg:max-w-[45%]"
-                                    )}
-                                >
-                                    {i.desc}
-                                </p>
+                                {i.desc}
+                            </p>
 
-                                {/* user-img */}
-                                <span className="w-28 h-28 inline-block rounded-full order-1 relative">
-                                    <Image 
-                                        alt="User Profile Picture Illustration"
-                                        src={i.img}
-                                        fill
-                                    />
-                                </span>
+                            {/* user-img */}
+                            <span className="w-28 h-28 inline-block rounded-full order-1 relative">
+                                <Image 
+                                    alt="User Profile Picture Illustration"
+                                    src={i.img}
+                                    fill
+                                    sizes="(max-width: 768px) 100vw,
+                                    (max-width: 1200px) 50vw,
+                                    33vw"
+                                />
+                            </span>
 
-                                {/* user-name */}
-                                <h1 className="mt-4 order-2">{i.name}</h1>
-                            </SwiperSlide>
-                        )}
-                    </Swiper>
-
-                {/* </div> */}
+                            {/* user-name */}
+                            <h1 className="mt-1 order-2 font-medium">{i.name}</h1>
+                        </SwiperSlide>
+                    )}
+                </Swiper>
                 {/* END: col-2 */}
 
-            </div>{/* END: container */}
+            </motion.div>{/* END: container */}
 
-        </section>
+        </motion.section>
 
     )
 }
