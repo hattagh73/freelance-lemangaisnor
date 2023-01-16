83df22c999c578856592a6ab7b9ef649d0a88f14
hattagh73 <>
2023-01-17 00:43:42 +0800

    UPDATE: testimony-design

--- components/sections/STestimony.tsx
+++ components/sections/STestimony.tsx
@@ -15,6 +15,9 @@ import 'swiper/css/navigation';
 import { motion } from 'framer-motion';
 import { staggerContainer, fadeInDown, fadeInUpwards } from '../../config/framer-variants';
 
+//* IMPORT: components
+import { Badge } from '../components';
+
 //* IMPORT: classnames
 import classNames from 'classnames';
 
@@ -32,44 +35,47 @@ const STestimony = ({testimony}: iSTestimony ) => {
             viewport={{once:true, amount: 0.6}}
             id="s_testimony"
             className={classNames(
-                "py-20",
+                "py-20 c-container grid",
+                "grid-cols-12 gap-x-2"
             )}
         >
-            {/* START: container */}
+
+            {/* START: testimony-col-1 */}
             <motion.div
                 variants={fadeInDown}
                 className={classNames(
-                    "c-container grid",
-                    "grid-cols-12 gap-x-5",
-                    ""
+                    "col-span-full",
                 )}
             >
-                {/* START: col-1 */}
-                <div
-                    className="col-span-12 lg:text-center"
-                >
 
-                    <h4 
-                        className="mini-title text-[#45ab41]"
-                    >{testimony.mini_title}
-                    </h4>
+                <div className="flex justify-between items-center">
 
                     <h1
                         className={classNames(
-                            "main-title text-[#052d1b]"
+                            "main-title"
                         )}
                     >{testimony.title}
                     </h1>
 
-                </div>{/* END: col-1 */}
-                
-                {/* START: col-2 */}
-                {/* <div
-                    className={classNames(
-                        "mt-28 col-span-12 flex flex-col items-center text-center"
-                    )}
-                > */}
+                    <Badge 
+                        alt="SVG icon image testimony"
+                        src={testimony.badge_img}
+                        mini_title={testimony.mini_title}
+                    />
+                    
+                </div>
 
+                <p className="text-clr-light-tertiary">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+
+            </motion.div>{/* END: testimony-col-1 */}
+
+            {/* START: testimony-col-2 */}
+            <motion.div
+                variants={fadeInDown}
+                className={classNames(
+                    "col-span-full",
+                )}
+            >
                 <Swiper
                     pagination={{
                         enabled: true,
@@ -77,25 +83,23 @@ const STestimony = ({testimony}: iSTestimony ) => {
                         bulletActiveClass: `${bulletActiveClass}`,
                     }}
                     modules={[Pagination, Navigation]}
-                    className="mt-28 col-span-12 max-w-full pb-6 px-3"
+                    className="mt-28 w-full pb-8"
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1,
+                            spaceBetween: 14,
+                            pagination: { enabled: true, clickable: true}
+                        }
+                    }}
                 >
                     {testimony.testimonials.map(i =>
                         <SwiperSlide 
                             key={i.id}
-                            className="flex flex-col items-center text-center"
+                            className="flex flex-col items-center "
                         >
-                            {/* user-testimony */}
-                            <p
-                                className={classNames(
-                                    "my-10 text-clr-light-tertiary order-3",
-                                    "lg:max-w-[55%]"
-                                )}
-                            >
-                                {i.desc}
-                            </p>
 
                             {/* user-img */}
-                            <span className="w-28 h-28 inline-block rounded-full order-1 relative">
+                            <span className="w-28 h-28 rounded-full relative">
                                 <Image 
                                     alt="User Profile Picture Illustration"
                                     src={i.img}
@@ -103,17 +107,32 @@ const STestimony = ({testimony}: iSTestimony ) => {
                                     sizes="(max-width: 768px) 100vw,
                                     (max-width: 1200px) 50vw,
                                     33vw"
+                                    className="object-cover"
                                 />
                             </span>
 
                             {/* user-name */}
-                            <h1 className="mt-1 order-2 font-medium">{i.name}</h1>
+                            <h1 className="-mt-1 font-medium">{i.name}</h1>
+
+                            {/* user-testimony */}
+                            <div
+                                className="bg-clr-light-secondary text-center mt-4 py-12 px-48 rounded"
+                            >
+                                <p
+                                    className={classNames(
+                                        "text-clr-light-tertiary",
+                                        // "lg:max-w-[55%]"
+                                    )}
+                                >
+                                    {i.desc}
+                                </p>
+                            </div>
+                            
                         </SwiperSlide>
                     )}
                 </Swiper>
-                {/* END: col-2 */}
 
-            </motion.div>{/* END: container */}
+            </motion.div>{/* END: testimony-col-2 */}
 
         </motion.section>
 
