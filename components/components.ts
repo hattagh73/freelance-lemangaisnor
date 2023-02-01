f71285d5168329482d02f145fe61748b81091c81
hattagh73 <>
2023-02-01 11:04:52 +0800

    UPDATE: promo-component

--- components/components.ts
+++ components/components.ts
@@ -9,6 +9,7 @@ export { default as SProduct } from './sections/SProduct';
 export { default as STestimony } from './sections/STestimony';
 export { default as SAbout } from './sections/SAbout';
 export { default as SLocation } from './sections/SLocation';
+export { default as SPromo } from './sections/SPromo';
 
 //* IMPORT: other
 export { default as Blob } from './others/Blob';
